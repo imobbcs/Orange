1bae9e9e4a147c03fecd3fdd2ea1a4da0a0d056c
Omi <imo.babics@gmail.com>
2026-03-10 14:51:02 +0000

    redirect root to app.html

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,9 @@
 import { GetServerSideProps } from 'next';
-import fs from 'fs';
-import path from 'path';
 
-export default function Home({ html }: { html: string }) {
-  return <div dangerouslySetInnerHTML={{ __html: html }} />;
-}
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const filePath = path.join(process.cwd(), 'public', 'app.html');
-  const html = fs.readFileSync(filePath, 'utf8');
-  // Extract body content only
-  const bodyMatch = html.match(/<body[^>]*>([\s\S]*)<\/body>/i);
-  const body = bodyMatch ? bodyMatch[1] : html;
-  return { props: { html: body } };
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  res.writeHead(302, { Location: '/app.html' });
+  res.end();
+  return { props: {} };
 };
+
+export default function Home() { return null; }
