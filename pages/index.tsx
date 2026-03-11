e51eff5a876b40c0b6a0fc125a0450cc37e67880
Omi <depresseddraper@pm.me>
2026-03-11 08:59:15 +0100

    Add server-side redirection to /app.html

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,7 @@
+import { GetServerSideProps } from 'next';
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  res.writeHead(302, { Location: '/app.html' });
+  res.end();
+  return { props: {} };
+};
 export default function Home() { return null; }
