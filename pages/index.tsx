0ce7475928ebb92e82565c9dea3d0974b469f4fd
Omi <depresseddraper@pm.me>
2026-03-13 21:32:31 +0100

    Set Cache-Control header before redirecting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
 import { GetServerSideProps } from 'next';
+
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
   res.writeHead(302, { Location: '/app.html' });
   res.end();
   return { props: {} };
 };
+
 export default function Home() { return null; }
