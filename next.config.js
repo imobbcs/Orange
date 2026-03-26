592958c81393322302307100838d8ba2db39a90b
Omi <depresseddraper@pm.me>
2026-03-26 11:32:37 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -38,6 +38,19 @@ const nextConfig = {
   async headers() {
     return [
       {
+        source: '/sitemap.xml',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/xml; charset=utf-8',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, s-maxage=86400',
+          },
+        ],
+      },
+      {
         source: '/app.html',
         headers: [
           {
