ebac195d63daa5ed274c5acee86d3f406ecd9feb
Omi <depresseddraper@pm.me>
2026-06-18 14:25:16 +0200

    Implement getServerSideProps for Home component
    Add server-side rendering for the Home component by reading HTML from a file.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,1 +1,11 @@
+export const getServerSideProps = async ({ res }) => {
+  res.setHeader('Cache-Control', 'no-store');
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  const fs = require('fs');
+  const path = require('path');
+  const html = fs.readFileSync(path.join(process.cwd(), 'public', 'app.html'), 'utf8');
+  res.end(html);
+  return { props: {} };
+};
+
 export default function Home() { return null; }
