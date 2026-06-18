0ce8e0922ce24abe5a7af3c64de865136441027a
Omi <depresseddraper@pm.me>
2026-06-18 14:22:21 +0200

    Remove server-side redirection logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,1 @@
-import { GetServerSideProps } from 'next';
-
-export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
-  res.writeHead(302, { Location: '/app.html' });
-  res.end();
-  return { props: {} };
-};
-
 export default function Home() { return null; }
